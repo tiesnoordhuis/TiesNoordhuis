894e45a5c9d37f27d09c3218b99e1b1646768e5e
Ties Noordhuis <tiesnoordhuis@gmail.com>
2018-05-24 13:05:30 +0200

    server2
    server response function maakt vars van de request voor later gebruik

--- server2.js
+++ server2.js
@@ -1,40 +1,47 @@
-const http = require("http");
+const httpModule = require("http");
 const moduleTies = require("./moduleTies");
-const url = require("url");
-const fs = require("fs");
-const stream = require("stream");
-const events = require("events");
-const eventEmitter = new events.EventEmitter();
+const urlModule = require("url");
+const fsModule = require("fs");
+const streamModule = require("stream");
+const eventsModule = require("events");
+const eventEmitter = new eventsModule.EventEmitter();
 
 const hostname = "localhost";
 const port = "3000";
 
-const server = http.createServer();
+const server = httpModule.createServer();
 
-server.on("request", (request, responce) => {
-  console.log("request made");
-  var url = request.url;
-  console.log(url);
-  var method = request.method;
-  console.log(method);
-  responce.end(url + method);
+server.on("request", (request, response) => {
+  request.on('error', (err) => {
+    // This prints the error message and stack trace to `stderr`.
+    console.error(err.stack);
+  });
+
+  response.on('error', (err) => {
+    // This prints the error message and stack trace to `stderr`.
+    console.error(err.stack);
+  });
+  var respnseData = createResponse(request);
+  response.end("");
 });
 
 server.listen(port, hostname, () => {
   console.log(`server running on ${hostname}:${port}`);
 });
 
-request.on('error', (err) => {
-  // This prints the error message and stack trace to `stderr`.
-  console.error(err.stack);
-});
 
-responce.on('error', (err) => {
-  // This prints the error message and stack trace to `stderr`.
-  console.error(err.stack);
-});
 
 eventEmitter.on('error', (err) => {
   // This prints the error message and stack trace to `stderr`.
   console.error(err.stack);
 });
+
+function createResponse(request) {
+  let { readable, domain, socket, connection, httpVersionMajor, httpVersionMinor, httpVersion, complete, headers, rawHeaders, trailers, rawTrailers, upgrade, url, method, statusCode, statusMessage, client} = request;
+  let { protocol, slashes, auth, host, port, hostname, hash, search, query, pathname, path, href} = urlModule.parse(url);
+  console.log(Object.keys(request));
+  console.log(Object.keys(urlModule.parse(url)));
+  console.log(url);
+  console.log(method);
+  console.log(protocol);
+}
